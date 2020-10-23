630a35b445f8bf6e64cd806340f580da5e0e41fb
vduseev <vagiz@duseev.com>
2020-10-23 04:09:58 +0200

    ✨ Add exceptions for adapters

---
+++ ilexconf/exceptions.py
@@ -0,0 +1,11 @@
+class UnknownDataSourceArgumentType(Exception):
+    pass
+
+class UnknownDataDestinationArgumentType(Exception):
+    pass
+
+class UnsupportedDataSourceType(Exception):
+    pass
+
+class UnsupportedDataDestinationType(Exception):
+    pass
