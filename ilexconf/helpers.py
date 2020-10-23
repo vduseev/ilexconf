91409f17e70013f883851f831643ebf33eb43d88
vduseev <vagiz@duseev.com>
2020-10-23 04:10:20 +0200

    ✨ Add adapther helpers

--- ilexconf/helpers.py
+++ ilexconf/helpers.py
@@ -1,8 +1,13 @@
-from typing import Dict
+from enum import Enum, auto
+from pathlib import Path
+from typing import Dict, Union, TextIO
+from io import IOBase
+
+from ilexconf.exceptions import UnknownDataSourceArgumentType, UnknownDataDestinationArgumentType
 
 
 def keyval_to_dict(key, value, prefix="", separator="__", lowercase=False) -> Dict:
-    """    Transform key-value into Mapping.    """
+    """Transform key-value into Mapping"""
 
     if prefix and not key.startswith(prefix):
         # if prefix is specified, then return nothing for keys without it
@@ -23,3 +28,36 @@ def keyval_to_dict(key, value, prefix="", separator="__", lowercase=False) -> Di
         return {parts[0]: value}
     else:
         return {parts: value}
+
+class DataSource(Enum):
+    STRING = auto()
+    FILE = auto()
+    PATH = auto()
+
+    @staticmethod
+    def determine(data: Union[str, TextIO, Path]):
+        if isinstance(data, Path):
+            return DataSource.PATH
+        elif isinstance(data, TextIO):
+            return DataSource.FILE
+        elif isinstance(data, str):
+            return DataSource.STRING
+        else:
+            raise UnknownDataSourceArgumentType()
+
+
+class DataDestination(Enum):
+    PATH = auto()
+    STRING_PATH = auto()
+    STREAM = auto()
+
+    @staticmethod
+    def determine(data: Union[Path, str, IOBase]):
+        if isinstance(data, Path):
+            return DataDestination.PATH
+        elif isinstance(data, IOBase):
+            return DataDestination.STREAM
+        elif isinstance(data, str):
+            return DataDestination.STRING_PATH
+        else:
+            raise UnknownDataDestinationArgumentType()
